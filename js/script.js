300b8b69072f23c3e4a0ea886d06643118325081
Murilo Maciel <murilomaciel06oficial@gmail.com>
2025-05-30 16:15:20 -0300

    atualizando script.js e style.css

--- js/script.js
+++ js/script.js
@@ -14,12 +14,21 @@ const jump = () => {
 const loop = setInterval(() => {
 
     const pipePosition = pipe.offsetLeft;
-    const marioPosition = mario.offsetBottom;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-    if (pipePosition <= 120) {
+    console.log(marioPosition);
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
 
         pipe.style.animation = 'none';
         pipe.style.left =  `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom =  `${pipePosition}px`;
+
+        mario.src = './images/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
     }
 
 }, 10);
